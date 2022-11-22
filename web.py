348a6571ee47700c1d328172edb403d56f20f198
Dimitrije Dolga <ddimitrije@gmail.com>
2022-11-22 14:07:36 +0100

    Add configuration files

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide")
 
 def add_todo():
     todo = st.session_state["new_todo"]+"\n"
@@ -15,11 +16,14 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase your <b>productivity</b>.",
+         unsafe_allow_html= True)
 
 #st.checkbox("Buy grocery.")
 #st.checkbox("Throw the trash.")
 #st.write("This app is to increas")
+st.text_input(label = "", placeholder = "Add new todo .....",
+              on_change=add_todo, key = 'new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key = todo)
@@ -29,8 +33,6 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label = "", placeholder = "Add new todo .....",
-              on_change=add_todo, key = 'new_todo')
 
 print("Hellow")
 
